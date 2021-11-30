47bda085f16f753a007ac30d0a9a76e3ee1f0de4
Frehzy <frehzzzy@gmail.com>
2021-11-30 16:28:12 +0500

    CustomStack

---
+++ StackTestsLib/GlobalException.cs
@@ -0,0 +1,36 @@
+﻿using CustomStack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace StackTestsLib
+{
+    [TestClass]
+    public class GlobalException
+    {
+        [ExpectedException(typeof(Exception), "Exception!")]
+        [TestMethod]
+        public void CreateStack_Exception() //Ошибка при создании слишком маленького stack
+        {
+            CustomStack<int> IntStack = new CustomStack<int>(-1);
+        }
+        [TestMethod]
+        public void CreateStack() //Ошибка при создании слишком большого stack
+        {
+            CustomStack<int> IntStack = new CustomStack<int>(0);
+            Assert.AreEqual(0, IntStack.MaxCount);
+        }
+        [ExpectedException(typeof(Exception), "Exception!")]
+        [TestMethod]
+        public void CreateStack_Exception2() //Ошибка при создании слишком большого stack
+        {
+            CustomStack<int> IntStack = new CustomStack<int>(32768);
+        }
+        [TestMethod]
+        public void CreateStack2() //Ошибка при создании слишком большого stack
+        {
+            CustomStack<int> IntStack = new CustomStack<int>(32767);
+            Assert.AreEqual(32767, IntStack.MaxCount);
+        }
+    }
+}
