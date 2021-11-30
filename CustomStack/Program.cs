81723f49e622a860176c9a09c765a71e19b153d5
Frehzy <frehzzzy@gmail.com>
2021-11-30 11:04:18 +0500

    CustomStack

---
+++ CustomStack/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> IntStack = new CustomStack<int>(3);
+            IntStack.Push(1);
+            IntStack.Push(5);
+            IntStack.Push(7);
+            foreach (var item in IntStack.GetStack())
+                Console.WriteLine(item);
+            IntStack.Pop();
+            foreach (var item in IntStack.GetStack())
+                Console.WriteLine(item);
+            IntStack.Clear();
+            foreach (var item in IntStack.GetStack())
+                Console.WriteLine(item);
+            IntStack.Push(1);
+            IntStack.Push(5);
+            foreach (var item in IntStack.GetStack())
+                Console.WriteLine(item);
+            Console.WriteLine(IntStack.Count);
+            IntStack.Pop();
+            Console.WriteLine(IntStack.Count);
+
+            Console.WriteLine("____________________");
+            IntStack.Dispose();
+            foreach (var item in IntStack.GetStack())
+                Console.WriteLine(item);
+
+            CustomStack<int> IntStack2 = new CustomStack<int>();
+            Console.WriteLine(IntStack2.Count);
+            IntStack2.Push(1);
+            IntStack2.Push(3);
+            Console.WriteLine(IntStack2.Count);
+            Console.WriteLine(IntStack2.MaxCount);
+            Console.WriteLine("____________________");
+            Console.WriteLine("____________________");
+            foreach (var item in IntStack2.GetStack())
+                Console.WriteLine(item);
+
+            var int3 = IntStack2.CopyTo(4);
+            Console.WriteLine("____________________");
+            Console.WriteLine("____________________");
+            foreach (var item in int3)
+                Console.WriteLine(item);
+            Console.WriteLine("____________________");
+            Console.WriteLine("____________________");
+        }
+    }
+}
---
+++ CustomStack/PrototypeStack.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    public interface IStack<T>
+    {
+        public abstract void Push(T item);
+        public abstract T Pop();
+        public abstract T Peek();
+        public abstract bool Contains(T item);
+        public abstract T[] CopyTo(int arrayIndex);
+        public abstract bool IsEmpty();
+        public abstract void Clear();
+        public abstract int Count { get; }
+        public abstract int MaxCount { get; }
+    }
+
+    public class CustomStack<T> : IStack<T>, IDisposable
+    {
+        private int Size; //размер стека на данный момент
+        private T[] Stack; //обобщённый тип (дженерик) стека
+        private int MaxStackSize; //максимальный размер стека
+        public T this[int index] //индексатор для стека
+        {
+            get 
+            {
+                if (index >= Stack.Length)
+                    throw new Exception("Выход за пределы стека");
+                return Stack[index]; 
+            }
+            private set { Stack[index] = value; }
+        }
+
+        public int Count => Size;
+        public int MaxCount => MaxStackSize;
+
+        public CustomStack()
+        {
+            Stack = new T[Int32.MaxValue / 2];
+            Size = 0;
+            this.MaxStackSize = Int32.MaxValue / 2;
+        }
+
+        public CustomStack(int MaxStackSize)
+        {
+            Stack = new T[MaxStackSize];
+            Size = 0;
+            this.MaxStackSize = MaxStackSize;
+        }
+
+        public void Push(T item)
+        {
+            if (Size == MaxStackSize)
+                throw new Exception("Переполнение стека");
+            Stack[Size++] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new Exception("Стек пуст");
+            var item = Stack[--Size];
+            Stack[Size] = default;
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new Exception("Стек пуст");
+            return Stack[Size - 1];
+        }
+
+        public bool Contains(T item)
+        {
+            return Stack.Contains(item);
+        }
+
+        public bool IsEmpty()
+        {
+            if (Size == 0)
+                return true;
+            else
+                return false;
+        }
+        public void Clear()
+        {
+            Array.Clear(Stack, 0, MaxStackSize);
+            Size = 0;
+        }
+
+        public T[] GetStack()
+        {
+            T[] tempArray = new T[Size];
+            Array.Copy(Stack, tempArray, Size);
+            Array.Reverse(tempArray, 0, tempArray.Length);
+            return tempArray;
+        }
+
+        public T[] CopyTo(int arrayIndex)
+        {
+            var oldStack = GetStack();
+            CustomStack<T> ResultStack;
+            if (Size >= arrayIndex)
+                ResultStack = new CustomStack<T>(Size);
+            else
+                ResultStack = new CustomStack<T>(arrayIndex);
+
+            for (int i = 0; i < arrayIndex; i++)
+            {
+                try
+                { ResultStack.Push(oldStack[i]); }
+                catch
+                { ResultStack.Push(default); }
+            }
+
+            Array.Reverse(ResultStack.Stack, 0, ResultStack.Size);
+            return ResultStack.GetStack();
+        }
+
+        public void Dispose()
+        {
+            Clear();
+            MaxStackSize = 0;
+            GC.Collect();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
