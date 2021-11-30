2d50aba2e25a98cab51ce2c1ca4f9a846dab54b4
Frehzy <frehzzzy@gmail.com>
2021-11-30 15:33:45 +0500

    CustomStack

--- CustomStack/Program.cs
+++ CustomStack/Program.cs
@@ -6,12 +6,7 @@ namespace CustomStack
     {
         static void Main(string[] args)
         {
-            CustomStack<string> IntStack = new CustomStack<string>(3);
-            IntStack.Push("Первое");
-            IntStack.Push("Второе");
-            var result = IntStack.CopyTo(2);
-            var result2 = IntStack.CopyTo(5);
-
+			
         }
     }
 }
