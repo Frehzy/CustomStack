291b66c06488e8b663ae5c26dabf017bd804a76f
Frehzy <frehzzzy@gmail.com>
2021-11-30 16:32:04 +0500

    Update GlobalException.cs

--- StackTestsLib/GlobalException.cs
+++ StackTestsLib/GlobalException.cs
@@ -13,24 +13,36 @@ namespace StackTestsLib
         public void CreateStack_Exception() //Ошибка при создании слишком маленького stack
         {
             CustomStack<int> IntStack = new CustomStack<int>(-1);
+            CustomStack<string> StringStack = new CustomStack<string>(-1);
+            CustomStack<long> LongStack = new CustomStack<long>(-1);
         }
         [TestMethod]
         public void CreateStack() //Ошибка при создании слишком большого stack
         {
             CustomStack<int> IntStack = new CustomStack<int>(0);
             Assert.AreEqual(0, IntStack.MaxCount);
+            CustomStack<string> StringStack = new CustomStack<string>(0);
+            Assert.AreEqual(0, StringStack.MaxCount);
+            CustomStack<long> LongStack = new CustomStack<long>(0);
+            Assert.AreEqual(0, LongStack.MaxCount);
         }
         [ExpectedException(typeof(Exception), "Exception!")]
         [TestMethod]
         public void CreateStack_Exception2() //Ошибка при создании слишком большого stack
         {
             CustomStack<int> IntStack = new CustomStack<int>(32768);
+            CustomStack<string> StringStack = new CustomStack<string>(32768);
+            CustomStack<long> LongStack = new CustomStack<long>(32768);
         }
         [TestMethod]
         public void CreateStack2() //Ошибка при создании слишком большого stack
         {
             CustomStack<int> IntStack = new CustomStack<int>(32767);
             Assert.AreEqual(32767, IntStack.MaxCount);
+            CustomStack<string> StringStack = new CustomStack<string>(32767);
+            Assert.AreEqual(32767, StringStack.MaxCount);
+            CustomStack<long> LongStack = new CustomStack<long>(32767);
+            Assert.AreEqual(32767, LongStack.MaxCount);
         }
     }
 }
